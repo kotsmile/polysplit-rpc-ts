750c3bd0836fbe082a15d288a7d2997134518a59
Gregory Rogachev <kotsmilewtm@gmail.com>
2024-01-28 19:45:22 +0100

    remove time limit

--- src/routing.ts
+++ src/routing.ts
@@ -5,7 +5,7 @@ import getStatusController from '@/controllers/get-status'
 import getStatsController from '@/controllers/v1/get-stats'
 import getStatsChainController from '@/controllers/v1/get-stats-chain'
 import { postChainControllerV1 } from '@/controllers/v1/post-chain'
-import { timeLimit } from './middlewares/time-limit'
+// import { timeLimit } from './middlewares/time-limit'
 
 export const routing: Routing = {
   status: getStatusController,
@@ -19,6 +19,6 @@ export const routing: Routing = {
 
 expressApp.post(
   '/v1/chain/:chainId',
-  timeLimit(100, 60 * 1000),
+  // timeLimit(100, 60 * 1000),
   postChainControllerV1
 )
