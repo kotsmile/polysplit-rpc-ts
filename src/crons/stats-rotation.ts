099f811928bec8f2bf5213a9bc8b54b8a06ffe97
Gregory Rogachev <kotsmilewtm@gmail.com>
2024-01-28 18:35:03 +0100

    update

--- src/crons/stats-rotation.ts
+++ src/crons/stats-rotation.ts
@@ -8,4 +8,4 @@ async function statsRotationCron(): Promise<boolean> {
   return response
 }
 
-createAndRunCronJob('0 0 */3 * *', statsRotationCron)
+createAndRunCronJob('*/5 * * * *', statsRotationCron)
